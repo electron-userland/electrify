c757f078452454af975c76eb9ad45865a5be4f20
develar <develar@gmail.com>
2017-06-28 20:47:58 +0200

    element col/row

--- src/renderer/element-ui.d.ts
+++ src/renderer/element-ui.d.ts
@@ -59,6 +59,78 @@ declare module "element-ui" {
     route: Route
 
     /** Is menu item disabled */
-    disabled: boolean
+    disabled?: boolean
+  }
+
+  // row.d.ts
+
+  /** Horizontal alignment of flex layout */
+  export type HorizontalAlignment = 'start' | 'end' | 'center' | 'space-around' | 'space-between'
+
+  /** vertical alignment of flex layout */
+  export type VertialAlignment = 'top' | 'middle' | 'bottom'
+
+  /** Row Layout Component */
+  export class Row extends ElementUIComponent {
+    /** Grid spacing */
+    gutter: number
+
+    /** Layout mode. You can use flex. Works in modern browsers */
+    type: string
+
+    /** Horizontal alignment of flex layout */
+    justify: HorizontalAlignment
+
+    /** vertical alignment of flex layout */
+    align: VertialAlignment
+  }
+
+  // col.d.ts
+
+  /** Responsive column props */
+  export interface ResponsiveColumnProperties {
+    /** Number of column the grid spans */
+    span: number,
+
+    /** Number of spacing on the left side of the grid */
+    offset: number
+  }
+
+  /** Responsive column property */
+  export type ResponsiveColumn = number | ResponsiveColumnProperties
+
+  /** Colunm Layout Component */
+  export class Col extends ElementUIComponent {
+    /** Number of column the grid spans */
+    span: number
+
+    /** Number of spacing on the left side of the grid */
+    offset: number
+
+    /** Number of columns that grid moves to the right */
+    push: number
+
+    /** Number of columns that grid moves to the left */
+    pull: number
+
+    /** <768px Responsive columns or column props object */
+    xs: ResponsiveColumn
+
+    /** ≥768px Responsive columns or column props object */
+    sm: ResponsiveColumn
+
+    /** ≥992 Responsive columns or column props object */
+    md: ResponsiveColumn
+
+    /** ≥1200 Responsive columns or column props object */
+    lg: ResponsiveColumn
+  }
+
+  // submenu.d.ts
+
+  /** Submenu Component */
+  export class Submenu extends ElementUIComponent {
+    /** Unique identification */
+    index: string
   }
 }
--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
-import { Menu, MenuItem } from 'element-ui'
+import { Menu, MenuItem, Submenu, Row, Col } from 'element-ui'
+// import "iview/dist/styles/iview.css"
+// import Checkbox from 'iview/src/components/checkbox'
 
 import App from './App.vue'
 import router from './router'
@@ -10,9 +12,13 @@ if (!process.env.IS_WEB) {
   Vue.use(require('vue-electron'))
 }
 
+Vue.use(Row)
+Vue.use(Col)
 Vue.use(Menu)
-Vue.use(MenuItem);
+Vue.use(Submenu)
+Vue.use(MenuItem)
 
+;
 (<any>Vue).http = (<any>Vue).prototype.$http = axios
 Vue.config.productionTip = false
 
